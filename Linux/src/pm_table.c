5612a3cbc9792aa032b61598fcf7a21edc03068e
Ilija <ilijaarsenijevic@gmail.com>
2026-05-01 00:20:15 +0200

    Added VDD_MISC address

--- Linux/src/pm_table.c
+++ Linux/src/pm_table.c
@@ -112,11 +112,11 @@ static const pm_family_map_t RAPHAEL_540104 = {
 /* Raphael 0x540004 (7950X/7900X 16-core, PM table 0x00540004) */
 static const pm_family_map_t RAPHAEL_540004 = {
     .named = {
-        {11, F_IOD_HOTSPOT}, {70, F_FCLK}, {74, F_UCLK}, {78, F_MCLK},
-        {82, F_VSOC}, {259, F_VDDG_IOD}, {261, F_VDDG_CCD},
-        {268, F_VDDP}, {271, F_VCORE}
+        {11, F_IOD_HOTSPOT}, {56, F_VDD_MISC}, {70, F_FCLK}, {74, F_UCLK},
+        {78, F_MCLK}, {82, F_VSOC}, {259, F_VDDG_IOD},
+        {261, F_VDDG_CCD}, {268, F_VDDP}, {271, F_VCORE}
     },
-    .named_count = 9,
+    .named_count = 10,
     .vid_idx = 275, .ppt_idx = 3, .socket_power_idx = 29,
     .core_voltage_start = 309, .core_temp_start = 325, .core_clock_start = 341,
     .max_cores = 16
